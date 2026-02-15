4958810cccf91b0bbdfaa3e4386aab2285a45a16
Lev <levdubovikov0808@gmail.com>
2026-02-15 10:05:58 +0300

    lesson14

---
+++ lesson14/providers.tf
@@ -0,0 +1,8 @@
+provider "openstack" {
+  auth_url    = var.auth_url
+  domain_name = var.domain_name
+  tenant_id   = var.tenant_id
+  user_name   = var.user_name
+  password    = var.password
+  region      = var.region
+}
